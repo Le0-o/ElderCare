bd0669fffabe00634574e595811f3974a12d50e6
Le0-o <163970466+Le0-o@users.noreply.github.com>
2024-09-03 00:29:02 +1000

    final

--- src/router/authenticate.js
+++ src/router/authenticate.js
@@ -20,17 +20,17 @@ import { ref } from 'vue';
 import router from '../router';
 
 const isAuthenticated = ref(false);
-const userRole = ref(null); // 添加用户角色变量
+const userRole = ref(null); // add user
 
 export function useAuth() {
   const login = (role) => {
     isAuthenticated.value = true;
-    userRole.value = role; // 设置用户角色
+    userRole.value = role; // set user
   };
 
   const logout = () => {
     isAuthenticated.value = false;
-    userRole.value = null; // 清除用户角色
+    userRole.value = null; // clear user
   };
 
   return { isAuthenticated, login, logout, userRole };
